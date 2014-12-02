02356a65499390515f36d1d660d8dcfe737d8da5
Teja Rupnik <teja@rupniktwins.com>
2014-12-02 15:19:19 +0100

    2. faza

--- lib/xml.r
+++ lib/xml.r
@@ -36,3 +36,40 @@ uvozi.obcine <- function() {
                           gsub("\\.", "", matrika[,2:5]))),
                     2, as.numeric), row.names=matrika[,1]))
 }
+
+library(XML)
+
+# Vrne vektor nizov z odstranjenimi začetnimi in končnimi "prazninami" (whitespace)
+# iz vozlišč, ki ustrezajo podani poti.
+stripByPath <- function(x, path) {
+  unlist(xpathApply(x, path,
+                    function(y) gsub("^\\s*(.*?)\\s*$", "\\1", xmlValue(y))))
+}
+
+uvozi.nacin <- function() {
+  url.nacin <- "http://pxweb.stat.si/pxweb/Dialog/SaveShow.asp"
+  doc.nacin <- htmlTreeParse(url.nacin, useInternalNodes=TRUE)
+  
+  # Poiščemo vse tabele v dokumentu
+  tabele <- getNodeSet(doc.nacin, "//table")
+  
+  # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
+  # trenutnim vozliščem
+  vrstice <- getNodeSet(tabele[[2]], "./tr")
+  
+  # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
+  # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
+  seznam <- lapply(vrstice[2:length(vrstice)], stripByPath, "./td")
+  
+  # Iz seznama vrstic naredimo matriko
+  matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
+  
+  # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
+  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[2]][[1]], ".//th"))
+  
+  # Podatke iz matrike spravimo v razpredelnico
+  return(data.frame(apply(gsub("\\*", "",
+                               gsub(",", ".",
+                                    gsub("\\.", "", matrika[,2:5]))),
+                          2, as.numeric), row.names=matrika[,1]))
+}
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,16 +1,42 @@
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+#uvoziDruzine <- function() {
+#  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
+#                      row.names = 1,
+#                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
+#                      fileEncoding = "Windows-1250"))
+#}
+# Zapišimo podatke v razpredelnico druzine.
+#cat("Uvažam podatke o družinah...\n")
+#druzine <- uvoziDruzine()
+
+
+#Tabela 1:
+uvoziPoLetih <- function(){
+  return(read.table("podatki/PoLetih.csv", sep = ";", as.is = TRUE,
+                    row.names = 3,
+                    col.names = 2009:2013,
+                    fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+cat("Uvažam letne podatke o stanovanjah (tabela 1)\n")
+leta <- uvoziPoLetih()
+
+#Tabela 2:
+uvoziPoInvestitorjih <- function(){
+  return(read.table("podatki/PoInvestitorjih.csv", sep = ";", as.is = TRUE,
+                    row.names = 2,
+                    col.names = c("Pravna oseba", "Fizična oseba"),
+                    fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o stanovanjih po investitorjih (tabela 2)\n")
+invest <- uvoziPoInvestitorjih()
+
+#Tabela 3:
+
+
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
