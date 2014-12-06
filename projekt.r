13b6cdd1972e2a986d991b73d2f1dfe0cad2f5bd
Teja Rupnik <teja@rupniktwins.com>
2014-12-06 13:06:41 +0100

    sumniki

--- projekt.r
+++ projekt.r
@@ -1,6 +1,6 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("uvoz/uvoz.r", fileEncoding="UTF-8")
-source("slike/grafi.r", fileEncoding="UTF-8")
+source("uvoz/uvoz.r", encoding="UTF-8")
+source("slike/grafi.r", encoding="UTF-8")
 
 # 3. faza: Analiza in vizualizacija podatkov
 #source("vizualizacija/vizualizacija.r")
