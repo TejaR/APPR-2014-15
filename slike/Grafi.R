d05b35a29f4832989d3f8b181c67ec28f0695a79
Teja Rupnik <teja@rupniktwins.com>
2014-12-06 13:04:16 +0100

    sumniki

--- slike/Grafi.R
+++ slike/Grafi.R
@@ -1,7 +1,7 @@
 pdf("slike/grafi.pdf",paper="a4")
 
 #leta
-barplot(as.matrix(leta), beside = TRUE, las = 2)
+barplot(as.matrix(leta), beside = TRUE, las = 2, legend = c("SLOVENIJA", "Pomurska", "Podravska", "Koroška", "Savinjska", "Zasavska", "Spodnjeposavska", "Jugovzhodna Slovenija", "Osrednjeslovenska", "Gorenjska", "Notranjsko-kraška", "Goriška", "Obalno-kraška"))
 
 #investitorji
 barplot(apply(invest[-1,], 1, c), beside = TRUE, las = 2, legend = c("Pravne osebe", "Fizične osebe"))
