629bb6fb6b96f93a3c4ef974cf1d0d263c1a069f
Teja Rupnik <teja@rupniktwins.com>
2014-12-06 12:52:23 +0100

    popravek grafa 1

--- slike/Grafi.R
+++ slike/Grafi.R
@@ -1,4 +1,4 @@
 pdf("slike/grafi.pdf",paper="a4")
 
 #investitorji
-barplot(apply(invest[-1,], 1, c), beside = TRUE, las = 2, legend = c("Pravne osebe", "Fizične osebe"))
+barplot(apply(invest[-1,], 1, c), beside = TRUE, las = 2, legend = c("Pravne osebe", "Fizične osebe"), fileEncoding="UTF-8")
