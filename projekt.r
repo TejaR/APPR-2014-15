75bd89f45e510adc2352a9b2963ba3527704eaaf
Teja Rupnik <teja@rupniktwins.com>
2014-12-07 16:08:15 +0100

    source

--- projekt.r
+++ projekt.r
@@ -1,6 +1,7 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
 source("uvoz/uvoz.r", encoding="UTF-8")
 source("slike/grafi.r", encoding="UTF-8")
+source("lib/xml.r", encoding="UTF-8")
 
 # 3. faza: Analiza in vizualizacija podatkov
 #source("vizualizacija/vizualizacija.r")
