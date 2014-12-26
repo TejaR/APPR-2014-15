bb40c4fe695eaf51b4af8899addc7046a317e1a7
Teja Rupnik <teja@rupniktwins.com>
2014-12-26 10:58:57 +0100

    3. faza

--- projekt.r
+++ projekt.r
@@ -3,7 +3,7 @@ source("uvoz/uvoz.r", encoding="UTF-8")
 source("slike/Grafi.R", encoding="UTF-8")
 source("lib/xml.r", encoding="UTF-8")
 
-# 3. faza: Analiza in vizualizacija podatkov
+ 3. faza: Analiza in vizualizacija podatkov
 #source("vizualizacija/vizualizacija.r")
 
 # 4. faza: Napredna analiza podatkov
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,21 +1,67 @@
 # 3. faza: Izdelava zemljevida
 
 # Uvozimo funkcijo za pobiranje in uvoz zemljevida.
+#source("lib/uvozi.zemljevid.r")
+
+# Uvozimo zemljevid.
+#cat("Uvažam zemljevid...\n")
+#obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
+#                          "obcine", "OB/OB.shp", mapa = "zemljevid",
+#                          encoding = "Windows-1250")
+
+# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
+#preuredi <- function(podatki, zemljevid) {
+#  nove.obcine <- c()
+#  manjkajo <- ! nove.obcine %in% rownames(podatki)
+#  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
+#  names(M) <- names(podatki)
+#  row.names(M) <- nove.obcine[manjkajo]
+#  podatki <- rbind(podatki, M)
+  
+#  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
+#  if (ncol(podatki) == 1) {
+#    out <- data.frame(out)
+#    names(out) <- names(podatki)
+#    rownames(out) <- rownames(podatki)
+#  }
+#  return(out)
+#}
+
+# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
+#druzine <- preuredi(druzine, obcine)
+
+# Izračunamo povprečno velikost družine.
+#druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
+#min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
+#max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+#cat("Rišem zemljevid...\n")
+#pdf("slike/povprecna_druzina.pdf", width=6, height=4)
+
+#n = 100
+#barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
+#plot(obcine, col = barve)
+
+#dev.off()
+
+
+# Uvozimo funkcijo za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r")
 
 # Uvozimo zemljevid.
 cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                          "obcine", "OB/OB.shp", mapa = "zemljevid",
+slo <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
+                          "Slovenija", "OB/OB.shp", mapa = "zemljevid",
                           encoding = "Windows-1250")
 
 # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
 preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
+  nove.slo <- c()
+  manjkajo <- ! nove.slo %in% rownames(podatki)
   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
   names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
+  row.names(M) <- nove.slo[manjkajo]
   podatki <- rbind(podatki, M)
   
   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
@@ -28,19 +74,19 @@ preuredi <- function(podatki, zemljevid) {
 }
 
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, obcine)
+invest <- preuredi(invest, slo)
 
 # Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+invest$povprecje <- apply(invest, 1, function(x) mean(x))
+min.povprecje <- min(invest$povprecje, na.rm=TRUE)
+max.povprecje <- max(invest$povprecje, na.rm=TRUE)
 
 # Narišimo zemljevid v PDF.
-cat("Rišem zemljevid...\n")
-pdf("slike/povprecna_druzina.pdf", width=6, height=4)
+cat("Rišem zemljevid slovenije...\n")
+pdf("slike/Slovenija.pdf")
 
 n = 100
-barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-plot(obcine, col = barve)
+barve = topo.colors(n)[1+(n-1)*(invest$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
+plot(slo, col = barve)
 
 dev.off()
