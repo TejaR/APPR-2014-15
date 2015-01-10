39761dd321f31f2930428d11b16e97fc51b0caae
Teja Rupnik <teja@rupniktwins.com>
2015-01-10 17:52:12 +0100

    zadjne poprave

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -78,13 +78,21 @@ min.povprecje <- min(invest$povprecje, na.rm=TRUE)
 max.povprecje <- max(invest$povprecje, na.rm=TRUE)
 norm.povprecje <- (invest$povprecje-min.povprecje)/(max.povprecje-min.povprecje)
 
-n = 100
-barve =rgb(1, 1, (n:1)/n)[unlist(1+(n-1)*norm.povprecje)]
-plot(regije, col = barve,bg="lightblue")
-text(coordinates(regije),labels=as.character(regije$NAME_1),cex=0.5)
+
+n <- 100
+barve <- rgb(1, 1, (n:1)/n)
+plot(regije, col = barve[unlist(1+(n-1)*norm.povprecje)],bg="lightblue")
+
+k <- 10 # število barv v legendi
+stopnje <- (1:k)*n/k
+legend("bottomright",
+       legend = round(seq(min.povprecje, max.povprecje,
+                          (max.povprecje-min.povprecje)/(k-1))),
+       fill = barve[stopnje], bg = "white")
+text(koordinate,rownames(koordinate),cex=0.5)
 title("Povprečno število stanovanj po regijah")
 LJUBLJANA <- regije$VARNAME_1 == "Ljubljana"
-points(coordinates(regije), pch = ifelse(LJUBLJANA, 18, 0), cex = ifelse(LJUBLJANA, 0.5 ,0), col = ifelse(LJUBLJANA, "red", "white"))
+points(14.51, 46.06, pch = 18, cex = 0.5, col = "red")
 
 
 dev.off()
@@ -93,21 +101,21 @@ pdf("slike/regije2.pdf")
 regije$stanovanja2008 <- zemljevid.leta[,3]
 print(spplot(regije, "stanovanja2008", col.regions  = topo.colors(50),
              main = "Število stanovanj po regijah (leto 2008)",
-             sp.layout = list(list("sp.text", coordinates(regije),as.character(regije$NAME_1), cex = 0.5))))
+             sp.layout = list(list("sp.text", koordinate,rownames(koordinate), cex = 0.5))))
 dev.off()
 
 pdf("slike/regije3.pdf")
 regije$stanovanja2011 <- zemljevid.leta[,6]
 print(spplot(regije, "stanovanja2011", col.regions  = topo.colors(50),
              main = "Število stanovanj po regijah (leto 2011)",
-             sp.layout = list(list("sp.text", coordinates(regije),as.character(regije$NAME_1), cex = 0.5))))
+             sp.layout = list(list("sp.text", koordinate,rownames(koordinate), cex = 0.5))))
 dev.off()
 
 pdf("slike/regije4.pdf")
 regije$stanovanja2013 <- zemljevid.leta[,"X2013"]
 print(spplot(regije, "stanovanja2013", col.regions  = topo.colors(50),
              main = "Število stanovanj po regijah (leto 2013)",
-             sp.layout = list(list("sp.text", coordinates(regije),as.character(regije$NAME_1), cex = 0.5))))
+             sp.layout = list(list("sp.text", koordinate,rownames(koordinate), cex = 0.5))))
 dev.off()
 
 # Uvozimo zemljevid.
@@ -139,13 +147,21 @@ min.povprecje <- min(prebivalstvo$povprecje, na.rm=TRUE)
 max.povprecje <- max(prebivalstvo$povprecje, na.rm=TRUE)
 norm.povprecje <- (prebivalstvo$povprecje-min.povprecje)/(max.povprecje-min.povprecje)
 
-n = 100
-barve =rgb(1, 1, (n:1)/n)[unlist(1+(n-1)*norm.povprecje)]
-plot(slo, col = barve,bg="lightblue")
-text(koordinate,labels=as.character(regije$NAME_1),cex=0.5)
+
+n <- 100
+barve <- rgb(1, 1, (n:1)/n)
+plot(slo, col = barve[unlist(1+(n-1)*norm.povprecje)],bg="lightblue")
+
+k <- 10 # število barv v legendi
+stopnje <- (1:k)*n/k
+legend("bottomright",
+       legend = round(seq(min.povprecje, max.povprecje,
+                          (max.povprecje-min.povprecje)/(k-1))),
+       fill = barve[stopnje], bg = "white")
+text(koordinate,rownames(koordinate),cex=0.5)
 title("Povprečno število prebivalcev po regijah")
 LJUBLJANA <- slo$VARNAME_1 == "Ljubljana"
-points(coordinates(slo), pch = ifelse(LJUBLJANA, 18, 0), cex = ifelse(LJUBLJANA, 0.5 ,0), col = ifelse(LJUBLJANA, "red", "white"))
+points(14.51, 46.06, pch = 18, cex = 0.5, col = "red")
 
 dev.off()
 
