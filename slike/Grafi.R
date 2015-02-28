5745e0290a181bef1f9892b6332421a3f7f5ca69
Teja Rupnik <teja@rupniktwins.com>
2015-02-28 15:46:51 +0100

    poročilo

--- slike/Grafi.R
+++ slike/Grafi.R
@@ -1,7 +1,7 @@
 pdf("slike/grafi.pdf",paper="a4")
 
 #leta
-barplot(as.matrix(leta), names.arg = 2008:2013, main="Gradnja stanovanj, med leti 2008 in 2013",
+barplot(as.matrix(leta), names.arg = 2008:2013, main="Gradnja stanovanj med leti 2008 in 2013",
         beside = TRUE, legend = rownames(leta),xlab = "Leto",
         args.legend = c(cex = 0.7), col = rainbow(12))
 
@@ -13,7 +13,7 @@ barplot(apply(invest[-1,], 1, c), beside = TRUE, names.arg = oznake[-1], las = 2
 
 #prebivalstvo
 barplot(as.matrix(prebivalstvo)/1000, names.arg = 2008:2013,
-        main="Tisoče prebivalcev, med leti 2008 in 2013",
+        main="Tisoče prebivalcev med leti 2008 in 2013",
         beside = TRUE, legend = rownames(prebivalstvo),xlab = "Leto",
         args.legend = c(cex = 0.7), col = rainbow(12))
 
